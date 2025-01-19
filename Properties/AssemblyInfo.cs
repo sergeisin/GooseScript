8189028efc98404d8a54bc3b724fc8a83c454443
sergeisin <sergei28.01.1994@gmail.com>
2025-01-19 17:33:31 +0300

    Add vs solution

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GooseScript")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GooseScript")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("20568a5a-3b5e-43b0-b409-8c9d2eac0a80")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Source/MainForm.Designer.cs
@@ -0,0 +1,35 @@
+﻿namespace GooseScript
+{
+    partial class MainForm
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // MainForm
+            // 
+            this.ClientSize = new System.Drawing.Size(684, 461);
+            this.MinimumSize = new System.Drawing.Size(500, 300);
+            this.Name = "MainForm";
+            this.Text = "GooseScript";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
