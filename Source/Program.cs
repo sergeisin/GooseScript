aff399fbf14d3e28d9b21c17ed3ca5ed75730a84
sergeisin <sergei28.01.1994@gmail.com>
2025-02-02 20:32:01 +0300

    High DPI support

--- Source/Program.cs
+++ Source/Program.cs
@@ -1,13 +1,23 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 using System.Windows.Forms;
 
 namespace GooseScript
 {
     internal static class Program
     {
+
+        [DllImport("user32.dll")]
+        private static extern bool SetProcessDPIAware();
+
         [STAThread]
         static void Main()
         {
+            if (Environment.OSVersion.Version.Major >= 6)
+            {
+                SetProcessDPIAware();
+            }
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
